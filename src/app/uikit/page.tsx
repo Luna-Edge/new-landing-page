6a57a815e78b4bfc44561243add8561def327ba4
alterproger <pensilproggertopg@gmail.com>
2024-08-21 10:45:28 +0300

    feat: implement ServiceCard component

--- src/app/uikit/page.tsx
+++ src/app/uikit/page.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 
+// components
 const Carrousel = dynamic(() => import("@/components/ui/Carrousel/Carrousel"), {
   ssr: false,
 });
@@ -10,10 +11,15 @@ import Input from "@/components/ui/Input/Input";
 import MarqueeSlider from "@/components/ui/MarqueeSlider/MarqueeSlider";
 import Textarea from "@/components/ui/Textarea/Textarea";
 import TestimonialCard from "@/components/ui/TestimonialCard/TestimonialCard";
+import ServiceCard from "@/components/ui/ServiceCard/ServiceCard";
 
+// constants
 import { MARQUEE_SLIDES_INFORMATION } from "@/components/ui/MarqueeSlider/constants";
 import { CARROUSEL_INFORMATION } from "@/components/ui/Carrousel/constants";
 import { TESTIMONIALS } from "./libs/utils/constants";
+
+// images
+import SoftwareDevelopment from "./libs/images/icons/SoftwareDevelopment.png";
 
 const UiKit = () => {
   return (
@@ -52,6 +58,26 @@ const UiKit = () => {
           {TESTIMONIALS.map((testimonial) => (
             <TestimonialCard key={testimonial.testimonial} {...testimonial} />
           ))}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            width: "50%",
+            aspectRatio: "1 / 1",
+          }}
+        >
+          <ServiceCard
+            title="Software Development"
+            image={{
+              src: SoftwareDevelopment,
+              styles: {
+                position: "absolute",
+                right: "12px",
+                bottom: "-5px",
+              },
+            }}
+          />
         </div>
       </div>
     </main>
