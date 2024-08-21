7c8fce64fa64ba15d166263cbed24e9607b173b4
alterproger <pensilproggertopg@gmail.com>
2024-08-21 13:05:04 +0300

    styles: update ScrollingText component`s styles

--- src/components/ui/MarqueeSlider/MarqueeSlider.tsx
+++ src/components/ui/MarqueeSlider/MarqueeSlider.tsx
@@ -11,33 +11,29 @@ import ScrollingText from "../ScrollingText/ScrollingText";
 import "./styles.scss";
 import { MARQUEE_SLIDES_INFORMATION } from "./constants";
 
-type MarqueeSliderProps = {
+interface MarqueeSliderProps {
   sliderData: typeof MARQUEE_SLIDES_INFORMATION;
-};
+}
 
-const MarqueeSlider = ({ sliderData }: MarqueeSliderProps) => {
+export default function MarqueeSlider({ sliderData }: MarqueeSliderProps) {
   return (
-    <>
-      <Swiper
-        slidesPerView={"auto"}
-        centeredSlides={true}
-        loop
-        autoplay={{ delay: 1, disableOnInteraction: true }}
-        modules={[Autoplay]}
-        speed={11000}
-        freeMode
-        spaceBetween={0}
-      >
-        {sliderData.map(({ id, altText, icon, text }) => {
-          return (
-            <SwiperSlide key={id}>
-              <ScrollingText altText={altText} text={text} icon={icon} />
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </>
+    <Swiper
+      slidesPerView={"auto"}
+      centeredSlides
+      loop
+      autoplay={{ delay: 1, disableOnInteraction: true }}
+      modules={[Autoplay]}
+      speed={11000}
+      freeMode
+      spaceBetween={0}
+    >
+      {sliderData.map(({ id, altText, icon, text }) => {
+        return (
+          <SwiperSlide key={id}>
+            <ScrollingText altText={altText} text={text} icon={icon} />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
-};
-
-export default MarqueeSlider;
+}
