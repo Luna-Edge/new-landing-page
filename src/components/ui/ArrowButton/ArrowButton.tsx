afbf6af95f5fba73af4c6db30def55554f274dbd
alterproger <pensilproggertopg@gmail.com>
2024-09-02 14:08:29 +0300

    Marge with master

---
+++ src/components/ui/ArrowButton/ArrowButton.tsx
@@ -0,0 +1,42 @@
+import { forwardRef, HTMLAttributes, useMemo } from "react";
+import styles from "./ArrowButton.module.scss";
+
+import Image from "next/image";
+import ArrowRight from "@/../public/icons/arrow-right.svg";
+import classNames from "@/utils/classNames/classNames";
+
+interface ArrowButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  direction: "left" | "right";
+}
+
+const ArrowButton = forwardRef<HTMLButtonElement, ArrowButtonProps>(
+  ({ direction, className = "", ...props }, ref) => {
+    const rotateDeg = useMemo(() => {
+      switch (direction) {
+        case "left":
+          return 180;
+        default:
+          return 0;
+      }
+    }, [direction]);
+
+    return (
+      <button
+        ref={ref}
+        className={classNames(styles.button, {}, [className])}
+        {...props}
+      >
+        <Image
+          className={styles.icon}
+          src={ArrowRight}
+          alt={`Arrow ${direction}`}
+          style={{ transform: `rotate(${rotateDeg}deg)` }}
+        />
+      </button>
+    );
+  }
+);
+
+ArrowButton.displayName = "ArrowButton";
+
+export default ArrowButton;
