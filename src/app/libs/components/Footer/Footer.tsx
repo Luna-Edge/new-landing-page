44dd5997220b0b023e4484d760685a61f4267d8f
alterproger <pensilproggertopg@gmail.com>
2024-09-04 16:01:27 +0300

    Add adaptive footer

--- src/app/libs/components/Footer/Footer.tsx
+++ src/app/libs/components/Footer/Footer.tsx
@@ -11,13 +11,20 @@ import {
 import InteractiveGridBackground from "@/components/ui/InteractiveGridBackground/InteractiveGridBackground";
 import { Canvas } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
+import { useResponsive } from "@/hooks/useResponsive";
 
 export default function Footer() {
   const boxShadowVal = useRef(50);
-  const { register, handleSubmit, formState: {errors} } = useForm<ContactFormSchema>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ContactFormSchema>({
     resolver: zodResolver(contactFormSchema),
   });
   const ref = useRef<HTMLElement>(null);
+
+  const [isMobile, isTablet] = useResponsive();
 
   const handleGoToTop = () => {
     window.scroll({ top: 0, left: 0, behavior: "smooth" });
@@ -69,8 +76,8 @@ export default function Footer() {
       <div className={styles.footer_FormWrapper}>
         <Canvas className={styles.footer_Grid} camera={{ position: [0, 0, 6] }}>
           <InteractiveGridBackground
-            gridSize={10}
-            boxSize={2}
+            gridSize={isTablet ? 9 : isMobile ? 7 : 10}
+            boxSize={isMobile ? 1 : 2}
             lineWidth={0.05}
             transitionSpeed={0.1}
           />
@@ -132,8 +139,12 @@ export default function Footer() {
       </ul>
       <h1 className={styles.footer_MainText}>Luna Edge</h1>
       <ul className={styles.footer_Copyright}>
-        <li className={styles.footer_CopyrightItem}>© 2024. All rights reserved</li>
-        <li className={styles.footer_CopyrightItem}>Made by Mariia Petrovych</li>
+        <li className={styles.footer_CopyrightItem}>
+          © 2024. All rights reserved
+        </li>
+        <li className={styles.footer_CopyrightItem}>
+          Made by Mariia Petrovych
+        </li>
       </ul>
     </footer>
   );
